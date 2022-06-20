8e05f041e787c995fefe31aef560dcfeb52640e8
Vinicius Souza <vinicius.souza@nre.tec.br>
2022-06-20 13:59:01 -0300

    init homeView

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,9 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+   background: #000002;
+   color: white;
 }
+
 </style>
