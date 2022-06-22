2f3382cc2eee1032be7c4b02eb186755300ce3f6
Vinicius Souza <vinicius.souza@nre.tec.br>
2022-06-22 11:21:03 -0300

    finish students view and staff view

--- src/views/StaffView.vue
+++ src/views/StaffView.vue
@@ -1,3 +1,60 @@
 <template>
-  <p>vizualizar personagens do staff</p>
+  <div class="staff-view">
+    <div class="container">
+      <h3 class="text-center title py-3">
+        Aqui você encontra informações sobre todos os trabalhadores de Hogowarts
+      </h3>
+      <div class="line mb-4"></div>
+      <div class="row" v-if="staff">
+        <div v-for="(staf, index) in staff.slice(0,inset)" :key="index" class="col-lg-3 mb-4">
+          <card-person :person="staf"/>
+        </div>
+      </div>
+    <div v-if="inset<= staff?.length" class="row justify-content-center my-3">
+      <div class="col-lg-4">
+        <button @click="seeMore" class="btn btn-gold w-100">Ver Mais</button>
+      </div>
+    </div>
+    </div>
+  </div>
 </template>
+<script>
+import CardPerson from "@/components/CardPerson.vue";
+import { mapState } from 'vuex';
+export default {
+  components: {
+    CardPerson,
+  },
+  data(){
+    return{
+      loading: false,
+      inset: 8
+    }
+  },
+  computed:{
+    ...mapState("characters",["staff"])
+  },
+  mounted(){
+    this.$store.dispatch("characters/getStaff").finally(()=>{
+      this.loading= false;
+    })
+  },
+  methods:{
+    seeMore(){
+      this.inset = this.inset+4;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.staff-view{
+  .btn-gold{
+    background: goldenrod;
+    color: white;
+    transition: filter 0.4s;
+    &:hover{
+      filter: brightness(80%);
+    }
+  }
+}
+</style>
--- src/views/StudentsView.vue
+++ src/views/StudentsView.vue
@@ -1,3 +1,59 @@
+
 <template>
-  <p>vizualizar personagens estudantes</p>
+  <div class="student-view">
+    <div class="container">
+      <h3 class="text-center title py-3">
+        Aqui você encontra informações sobre todos os estudantes de Hogowarts
+      </h3>
+      <div class="line mb-4"></div>
+      <div class="row" v-if="students">
+        <div v-for="(student, index) in students.slice(0,inset)" :key="index" class="col-lg-3 mb-4">
+          <card-person :person="student"/>
+        </div>
+      </div>
+    <div v-if="inset<= students?.length" class="row justify-content-center my-3">
+      <div class="col-lg-4">
+        <button @click="seeMore" class="btn btn-gold w-100">Ver Mais</button>
+      </div>
+    </div>
+    </div>
+  </div>
 </template>
+<script>
+import CardPerson from "@/components/CardPerson.vue";
+import { mapState } from 'vuex';
+export default {
+  components: {
+    CardPerson,
+  },
+  data(){
+    return{
+      loading: false,
+      inset: 8
+    }
+  },
+  computed:{
+    ...mapState("characters",["students"])
+  },
+  mounted(){
+    this.$store.dispatch("characters/getStudents")
+  },
+  methods:{
+    seeMore(){
+      this.inset = this.inset+8;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.student-view{
+  .btn-gold{
+    background: goldenrod;
+    color: white;
+    transition: filter 0.4s;
+    &:hover{
+      filter: brightness(80%);
+    }
+  }
+}
+</style>
